d39531f2ac77ad799450cbc82e35a8ae63efe606
obmbane <obmbaneaws@gmail.com>
2024-12-04 18:25:02 +0200

    additional chages

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import glob
+from fpdf import FPDF
 
 #create list of file names
 filepaths = glob.glob('excel_templates/*.xlsx')
@@ -7,4 +8,11 @@ print(filepaths)
 
 for filepath in filepaths:
     df = pd.read_excel(filepath,sheet_name='Sheet 1')
-    print(df)
+    invoice_list = filepath.split('-')
+    pdf = FPDF(orientation='P',unit='mm',format='A4')
+    pdf.add_page()
+    pdf.set_font(family='Times',size=20,style='B')
+    pdf.cell(w=60,h=20,txt=f'Invoice Number: {invoice_list[0]}')
+    pdf.output(f'{filepath}.pdf')
+
+    #print(df)
