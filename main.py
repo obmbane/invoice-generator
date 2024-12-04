699fb6713600ae6a513980a01b90aa2f049ea3ac
obmbane <obmbaneaws@gmail.com>
2024-12-04 19:05:31 +0200

    created pdf file with invoice number and date

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import pandas as pd
 import glob
 from fpdf import FPDF
+from pathlib import Path
 
 #create list of file names
 filepaths = glob.glob('excel_templates/*.xlsx')
-print(filepaths)
+
 
 for filepath in filepaths:
     df = pd.read_excel(filepath,sheet_name='Sheet 1')
-    invoice_list = filepath.split('-')
+    file_name = Path(filepath).stem
+    invoice_num = file_name.split('-')[0]
+    Invoice_date = file_name.split('-')[1]
     pdf = FPDF(orientation='P',unit='mm',format='A4')
     pdf.add_page()
     pdf.set_font(family='Times',size=20,style='B')
-    pdf.cell(w=60,h=20,txt=f'Invoice Number: {invoice_list[0]}')
-    pdf.output(f'{filepath}.pdf')
+    pdf.cell(w=60,h=20,txt=f'Invoice Number: {invoice_num}',ln=1)
+    pdf.set_font(family='Times',size=14,style='B')
+    pdf.cell(w=60,h=14,txt=f'Date: {Invoice_date}')
+
+    pdf_name = filepath.split('/')[1][-4]
+    pdf.output(f'PDF_Invoices/{file_name}.pdf')
 
     #print(df)
